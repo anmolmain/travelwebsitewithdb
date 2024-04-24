49b8b171f7e94f5538926839cc9457311d6e0ac8
mainnanmol07 <mainnabhi07@gmail.com>
2024-04-24 12:23:51 +0530

    first commit

--- travelwebsitedb/urls.py
+++ travelwebsitedb/urls.py
@@ -5,12 +5,12 @@ from eventhorizon import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home,name="home"),
-    path('trips', views.trips,name="trips"),
-    path('checkout', views.checkout,name="checkout"),
-    path('contactUs', views.contactUs,name="contactUs"),
-    path('save_booking/', views.save_booking, name='save_booking'),
-    path("register", views.register, name="register"),
-    path("login", views.login, name="login"),
-    path("logout", views.logout, name="logout"),
+    path('trips/', views.trips,name="trips"),
+    path('checkout/', views.checkout,name="checkout"),
+    path('contactUs/', views.contactUs,name="contactUs"),
+    path('save_booking//', views.save_booking, name='save_booking'),
+    path("register/", views.register, name="register"),
+    path("login/", views.login, name="login"),
+    path("logout/", views.logout, name="logout"),
 
 ]
